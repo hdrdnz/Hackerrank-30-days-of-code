7d64c390c082da91bb04181489d14f720421b9a5
hdrdnz <h_durudeniz@hotmail.com>
2023-04-14 22:11:10 +0300

    add day21-generics

---
+++ Solutions/Day21-Generics/src/Generics.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Printer <T> {
+
+    /**
+     *    Method Name: printArray
+     *    Print each element of the generic array on a new line. Do not return anything.
+     *    @param A generic array
+     **/
+
+    // Write your code here
+
+}
+
+public class Generics {
+
+
+}
